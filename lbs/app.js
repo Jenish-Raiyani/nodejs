a8472a90c69acd23b68df3eefa60b220651b4860
Jenish Raiyani <66068702+Jenish-Raiyani@users.noreply.github.com>
2021-11-11 00:13:55 +0530

    lbs

---
+++ lbs/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const dotenv =require('dotenv');
+const path= require('path');
+dotenv.config({path:'./.env'});
+
+const db= mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABSE,
+
+});
+
+
+
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set('view engine','hbs');
+db.connect((error)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log("Database Connected")
+    }
+
+});
+
+//routes
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+/*
+app.get('/', (req,res)=>{
+
+    res.render('index')
+});
+app.get('/register', (req,res)=>{
+
+    res.render('register')
+});
+*/
+app.listen(3000,()=>{
+    console.log("Server started")
+})
