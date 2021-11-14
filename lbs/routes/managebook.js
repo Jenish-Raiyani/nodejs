2cab01443c28c4b3e9650bc0f1a811911328ec0e
Jenish Raiyani <66068702+Jenish-Raiyani@users.noreply.github.com>
2021-11-14 23:02:01 +0530

    blocation

--- lbs/routes/managebook.js
+++ lbs/routes/managebook.js
@@ -27,6 +27,7 @@ router.get('/booklocation/managelocation/:id',(req, res)=>{
    
     
     var BookId= req.params.id;
+    console.log(BookId);
     
   
     var sql=`SELECT * FROM books WHERE id=${BookId}`;
@@ -37,18 +38,35 @@ router.get('/booklocation/managelocation/:id',(req, res)=>{
         totalbook=row.Total_copies;
         
       });
+      /*
+      var sqlq=`SELECT * FROM booklocation WHERE bookid=${BookId}`;
+          db.query(sqlq, function (err, blocation) {
+          if (err) throw err;
+            console.log(blocation);
+          //  return res.render('managelocation', { title: 'Book List',booklocation:blocation});
+         
+           
+          callback();
+        });
+        db.end(); */
+       
       var arr = [];
       
         for (var i = 1; i <= totalbook; i++) arr.push(""+i);
-       
+        
+      console.log(arr);
       return res.render('managelocation', { title: 'Book List', Bookdata: data ,totalbook:arr});
+      
     });
+    
  });
 
 
  router.post('/booklocation/managelocation/:id', function(req, res, next) {
+   
 
    var BookId= req.params.id;
+   /*
    var sql=`SELECT * FROM books WHERE id=${BookId}`;
    db.query(sql, function (err, data) {
      if (err) throw err;
@@ -62,28 +80,52 @@ router.get('/booklocation/managelocation/:id',(req, res)=>{
       // console.log(isbn,title,authors,publisher,url);
        
      });
-    });
+    });*/
     console.log(req.body)
     var isbn= req.body.isbn;
     var title= req.body.title;
     var authors= req.body.authors;
     var location = req.body.location;
     var status = req.body.status;
-    var publisher = "jk";
-    var url= "http"
+    var publisher = req.body.publisher;
+    var url= req.body.url;
+    var rowid=req.body.rowid;
+    
   //   console.log(status,location)
-    db.query('INSERT INTO booklocation SET ?',{bookid:BookId,ISBN:isbn,title:title,authors:authors,publisher:publisher,url:url,location:location,status:status},(error,results)=>{
-      if(error){
-          console.log(error);
-      }
-      else{
-          //res.redirect('/addbook');
-          console.log("Location Added");
-        
-            
-      } 
-  });
+  db.query('SELECT rowid FROM booklocation WHERE rowid = ?', [rowid], async (error,result)=>{
+    if(error){
+        console.log(error);
+    }
+   
+    if( result.length > 0){
+      res.redirect(`/booklocation/managelocation/${BookId}`);
+       ;
+    }
+    else {
+      db.query('INSERT INTO booklocation SET ?',{rowid:rowid,bookid:BookId,ISBN:isbn,title:title,authors:authors,publisher:publisher,url:url,location:location,status:status},(error,results)=>{
+        if(error){
+            console.log(error);
+        }
+        else{
+            res.redirect(`/booklocation/managelocation/${BookId}`);
   
+            console.log("Location Added");
+            return res.render('managelocation',{
+              change: "welcome"
+            });
+             
+        } 
+  
+    });
+    
+
+    }
+    
+
+   
+}); 
+  
+    
     
 
 });
